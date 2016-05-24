022f8449dc05e629a4b9e740410e63eb6baf6dd7
sky999999 <stevenli1141@gmail.com>
2016-05-23 20:47:40 -0400

    Revamped index.ejs

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,4 +1,46 @@
 //Javascript for loading the main chat board
+
+var app = {
+  initialize: function(){
+    this.currentroom = 'Main';
+  },
+  connect : function(){
+    var constructSocket = function(){
+      return new SockJS('/tempest');
+    }
+    this.socket = constructSocket();
+
+    var socketopened = false;
+
+    this.socket.onopen = function(){
+      socketopened = true;
+    };
+    this.socket.onmessage = function(message){
+      this.receive(message.data);
+    };
+    this.socket.onclose = function(){
+      //
+    }
+  },
+
+  send: function(data, room){
+    this.socket.send(data);
+  },
+
+  receive: function(data){
+    var message = JSON.parse(data);
+    if(message.roomid !== this.currentroom){
+      return;
+    }
+
+    if(message.type === 'post'){
+      $('#messagebox').append('<p>' + message.text + '</p>');
+      $('#messagebox').scrollTop(10000);
+      console.log('message', message.text);
+    }
+  }
+};
+
 var sock = new SockJS('/tempest');
 sock.onopen = function() {
   console.log('open');
