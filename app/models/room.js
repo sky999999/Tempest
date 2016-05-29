05b9eabbe4f6daea8c307e67c97d66e7645b88cc
sky999999 <stevenli1141@gmail.com>
2016-05-29 17:41:32 -0400

    Fixed time displays

---
+++ app/models/room.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+
+var schema = mongoose.Schema({
+  name: String,
+  access: {type: String, default: 'Public'},
+  date_created: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Room', schema);
